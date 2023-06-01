001211eb9d9cd44927f34252ab34d91a7836676f
Marielle Lunnan <marielle.lunnan@learnet.se>
2023-06-01 13:13:28 +0200

    init

--- LetsPack/Services/ItemService.cs
+++ LetsPack/Services/ItemService.cs
@@ -36,5 +36,41 @@ namespace LetsPack.Services
                 throw new Exception($"No user with email: " + input.UserName + " could be found");
             }
         }
+        public async Task EditItem(ItemInput input, int id)
+        {
+            try
+            {
+                var item = await _ctx.items!.FindAsync(id);
+                if(item == null)
+                {
+                    throw new Exception($"No item with id: {id} could be found");
+                }
+                item.Name = input.Name;
+                item.Description = input.Description;
+                item.Category = input.Category;
+                item.Store = input.Store;
+                item.Price = input.Price;
+
+                _ctx.items.Update(item);
+                await _ctx.SaveChangesAsync();
+            }
+            catch
+            {
+                throw new Exception($"No item with id: {id} could be found");
+            }
+        }
+        public async Task RemoveItem(int id)
+        {
+            var item = await _ctx.items!.FindAsync(id);
+            if(item != null)
+            {
+                _ctx.items.Remove(item);
+                await _ctx.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception($"No item with id: {id} could be found");
+            }
+        }
     }
 }
