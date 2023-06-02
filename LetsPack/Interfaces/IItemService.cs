06d493ace915f30a770e712abe342fc5bf10cbdc
Marielle Lunnan <marielle.lunnan@learnet.se>
2023-06-02 09:25:12 +0200

    itemService

--- LetsPack/Interfaces/IItemService.cs
+++ LetsPack/Interfaces/IItemService.cs
@@ -1,6 +1,13 @@
-﻿namespace LetsPack.Interfaces
+﻿using LetsPack.DTOs;
+
+namespace LetsPack.Interfaces
 {
     public interface IItemService
     {
+        Task AddItem(ItemInput input);
+        Task EditItem(ItemInput input, int id);
+        Task RemoveItem(int id);
+        Task<ItemInput> GetItemById(int id, string userName);
+        Task<ItemInput> GetAllItems(string userName);
     }
 }
